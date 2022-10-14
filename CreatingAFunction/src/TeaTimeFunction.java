0a95ecf7a3b7a215769811a38f744524c7f5e8f3
Craig <craig.woodcock89@hotmail.co.uk>
2022-10-14 22:51:38 +0100

    TeaTimeFunction

--- CreatingAFunction/src/TeaTimeFunction.java
+++ CreatingAFunction/src/TeaTimeFunction.java
@@ -9,7 +9,15 @@ public class TeaTimeFunction {
         System.out.println("it's tea time!! lets have cake");
     }
     public static void main(String[] args) {
-        announceTeaTme();
+        Scanner reader = new Scanner(System.in);
+        System.out.println("would you like to run the 'Announce Tea Time' Function");
+        System.out.println("Yes or No?");
+        String answer = reader.next().toLowerCase().trim();
 
+        if (answer.equals("yes")) {
+            announceTeaTme();
+        }else {
+            System.out.println("It appears you have come to the wrong place. This programme runs 'Announce Tea Time' Only!");
+        }
     }
 }
